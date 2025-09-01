06262bd3bffc42fe0d545f65198cc6a9d77a5702
annaseregina <s-Anna.Seregina@Lwtech.Edu>
2025-09-01 15:40:40 -0700

    Serve /media files in production via django.views.static.serve

--- recipesite/urls.py
+++ recipesite/urls.py
@@ -16,21 +16,21 @@ Including another URLconf
 """
 # recipesite/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.http import HttpResponse
-
 
 def healthz(_request):
     return HttpResponse("ok")
 
-
 urlpatterns = [
     path("healthz/", healthz),
     path("admin/", admin.site.urls),
-    path("", include("recipes.urls")),  # delegate to app
+    path("", include("recipes.urls")),
 ]
 
-# Serve user-uploaded media files (hero images)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve user-uploaded media files even if DEBUG=False
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
