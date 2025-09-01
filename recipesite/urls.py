933efda286f4f10827c78f7c01df35c51eee6b9e
annaseregina <s-Anna.Seregina@Lwtech.Edu>
2025-08-31 20:12:11 -0700

    Serve MEDIA files in production on Render

--- recipesite/urls.py
+++ recipesite/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     path("", include("recipes.urls")),  # delegate to app
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve user-uploaded media files (hero images)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
