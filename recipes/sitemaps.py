f9f4c816d0caf643dbaeb3bd99cd8497a7e14f9e
annaseregina <s-Anna.Seregina@Lwtech.Edu>
2025-09-03 12:12:19 -0700

    Optimize sitemap frequencies and priorities

--- recipes/sitemaps.py
+++ recipes/sitemaps.py
@@ -3,22 +3,35 @@ from django.urls import reverse
 from .models import Recipe
 
 class RecipeSitemap(Sitemap):
-    changefreq = "weekly"
+    changefreq = "monthly"   # recipes rarely change
     priority = 0.8
 
     def items(self):
         return Recipe.objects.all()
 
     def location(self, obj):
-        # detail page with a slug field
-        return f"/r/{obj.slug}/"
+        return reverse("recipe_detail", args=[obj.slug])
+
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.5
-    changefreq = "monthly"
-
     def items(self):
-        return ["home", "contact", "nutrition", "about"]
+        return ["home", "nutrition", "about", "contact"]
 
     def location(self, item):
         return reverse(item)
+
+    def changefreq(self, item):
+        if item == "home":
+            return "daily"   # homepage changes often
+        elif item == "nutrition":
+            return "weekly"  # updates with new recipes
+        else:
+            return "yearly"  # about/contact rarely change
+
+    def priority(self, item):
+        if item == "home":
+            return 1.0
+        elif item == "nutrition":
+            return 0.7
+        else:
+            return 0.5
